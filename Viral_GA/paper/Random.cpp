e8d526646d8a341287c82efe80276f55904c3abb
fog856 <45534739+fog856@users.noreply.github.com>
2020-06-10 21:39:58 +0530

    Add files via upload

---
+++ Viral_GA/paper/Random.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+vector <vector <lli> > md[20];
+int main(){
+    lli n;
+    cin>>n;
+    lli origN = n;
+    n = 
+    vector <lli> temp;
+    lli t;
+    for(lli i = 0; i<n; i++){
+        cin>>t;
+        temp.push_back(t);
+    }
+    md.push_back(temp);
+    while(n > 0){
+
+    }
+
+}
+
